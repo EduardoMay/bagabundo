f7a4d77f1e82cdc7f1abcac9f947012c8ecd3c93
Eduardo May <eduardomay_20400@outlook.com>
2019-03-14 12:15:42 -0500

    acomodando el about

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -12,30 +12,39 @@ $this->params['breadcrumbs'][] = $this->title;
   <h1>Bagabundo!</h1>
 
   <div class="row">
-    <div class="col-sm-6">
+    <div class="col-sm-12">
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">   </h5>
           <p class="card-text">El objetivo principal de este proyecto es crear una página web para dar 
           a conocer a los clientes/usuarios una gran variedad de viajes a los lugares más visitados del mundo.</p>  
         </div>
       </div>
     </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-6">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">  </h5>
-            <p class="card-text"> Se desea brindarle a la gente una página de búsqueda de servicios de hospitalidad y 
-            viajes con el fin de que las personas encuentren un servicio a su gusto y economía de la gente.</p> 
-          </div>
-        </div>
-      </div>   
-    
-  
+  </div>
   <div class="row">
-    <div class="col-sm-6">
+    <div class="col-sm-12">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">  </h5>
+          <p class="card-text"> Se desea brindarle a la gente una página de búsqueda de servicios de hospitalidad y 
+          viajes con el fin de que las personas encuentren un servicio a su gusto y economía de la gente.</p> 
+        </div>
+      </div>
+    </div>
+  </div> 
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">  </h5>
+          <p class="card-text">Bagabundo ofrece una aplicación gratuita para su producto de Búsqueda de hoteles,
+            se basa en un sistema en que el cliente hace la reserva del alojamiento u hotel y paga por tarjeta de crédito.  </p>
+          <p class="card-text"> Por lo tanto funciona como un portal web de viajes que genera una conexión inmediata de los consumidores con los sitios de viajes. 
+            Es un conjunto de redes que permite a distintos hoteles y alojamientos ofrecer su oferta en tiempo real en internet </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-12">
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">  </h5>
@@ -48,16 +57,7 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
       </div>
     </div>
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">  </h5>
-          <p class="card-text">Bagabundo ofrece una aplicación gratuita para su producto de Búsqueda de hoteles,
-            se basa en un sistema en que el cliente hace la reserva del alojamiento u hotel y paga por tarjeta de crédito.  </p>
-          <p class="card-text"> Por lo tanto funciona como un portal web de viajes que genera una conexión inmediata de los consumidores con los sitios de viajes. 
-            Es un conjunto de redes que permite a distintos hoteles y alojamientos ofrecer su oferta en tiempo real en internet </p>
-        </div>
-      </div>
-    </div>
   </div>
+    
+  
 </div>
