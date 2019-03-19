f97dc90ef470f64270de52827a83b3884d24a806
Eduardo May <eduardomay_20400@outlook.com>
2019-03-19 11:54:13 -0500

    corrigiendo vista

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -52,47 +52,48 @@ $this->title = 'My Yii Application';
         </div> -->
 
         <h1>Automoviles</h1>
-        <?php foreach ($cars as $car): ?>
-            <div class="card" style="width: 18rem;">
-              <img src="https://cadencestorage.blob.core.windows.net/mondial/PartImages/SmallImages/No_Image.jpg" class="card-img-top" alt="<?= Html::encode("{$car->model}") ?>">
-              <div class="card-body">
-                <h5 class="card-title">
-                    <?= Html::encode("{$car->model}") ?>
-                </h5>
-                <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Tipo de auto: <?= Html::encode("{$car->type_car}") ?></li>
-                <li class="list-group-item">Color: <?= Html::encode("{$car->color}") ?></li>
-                <li class="list-group-item">Estado: <?= Html::encode("{$car->state}") ?></li>
-                <li class="list-group-item">Precio: $<?= Html::encode("{$car->price}") ?></li>
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div>
-        <?php endforeach; ?>
+        <div class="card" style="width: 18rem;">
+            <?php foreach ($cars as $car): ?>
+                  <img src="https://cadencestorage.blob.core.windows.net/mondial/PartImages/SmallImages/No_Image.jpg" class="card-img-top" alt="<?= Html::encode("{$car->model}") ?>">
+                  <div class="card-body">
+                    <h5 class="card-title">
+                        <?= Html::encode("{$car->model}") ?>
+                    </h5>
+                    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                  </div>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Tipo de auto: <?= Html::encode("{$car->type_car}") ?></li>
+                    <li class="list-group-item">Color: <?= Html::encode("{$car->color}") ?></li>
+                    <li class="list-group-item">Estado: <?= Html::encode("{$car->state}") ?></li>
+                    <li class="list-group-item">Precio: $<?= Html::encode("{$car->price}") ?></li>
+                  </ul>
+                  <div class="card-body">
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                  </div>
+            <?php endforeach; ?>
+        </div>
+
         <hr>
         <h1>Hoteles</h1>
-        <?php foreach ($hotels as $hotel): ?>
-            <div class="card" style="width: 18rem;">
-              <img src="https://cadencestorage.blob.core.windows.net/mondial/PartImages/SmallImages/No_Image.jpg" class="card-img-top" alt="<?= Html::encode("{$hotel->hotel_name}") ?>">
-              <div class="card-body">
-                <h5 class="card-title">
-                    <?= Html::encode("{$hotel->hotel_name}") ?>
-                </h5>
-                <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Estrella: <?= Html::encode("{$hotel->stars}") ?></li>
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div>
-        <?php endforeach; ?>
+        <div class="card" style="width: 18rem;">
+            <?php foreach ($hotels as $hotel): ?>
+                  <img src="https://cadencestorage.blob.core.windows.net/mondial/PartImages/SmallImages/No_Image.jpg" class="card-img-top" alt="<?= Html::encode("{$hotel->hotel_name}") ?>">
+                  <div class="card-body">
+                    <h5 class="card-title">
+                        <?= Html::encode("{$hotel->hotel_name}") ?>
+                    </h5>
+                    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                  </div>
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Estrella: <?= Html::encode("{$hotel->stars}") ?></li>
+                  </ul>
+                  <div class="card-body">
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                  </div>
+            <?php endforeach; ?>
+        </div>
         
         <?= LinkPager::widget(['pagination' => $pagination,]); ?>
     </div>
