4a33c175715118e864687b3114cd78e773499077
maayraa <maaayraaa123@gmail.com>
2019-03-20 21:10:47 -0500

    #14 Vista Hotel

--- frontend/controllers/HotelController.php
+++ frontend/controllers/HotelController.php
@@ -2,11 +2,31 @@
 
 namespace frontend\controllers;
 
-class HotelController extends \yii\web\Controller
+use Yii;
+use yii\web\Controller;
+use common\models\Hotel;
+use yii\data\Pagination;
+
+class HotelController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $queryHotel = Hotel::find();
+
+        $pagination = new Pagination([
+            'defaultPageSize' => 20,
+            'totalCount' => $queryHotel->count(),
+        ]);
+
+        $hotels = $queryHotel->orderBy('hotel_name')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        return $this->render('index',[
+            'hotels' => $hotels,
+            'pagination' => $pagination,
+        ]);
     }
 
 }
--- frontend/views/hotel/index.php
+++ frontend/views/hotel/index.php
@@ -1,9 +1,24 @@
 <?php
 /* @var $this yii\web\View */
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
 ?>
-<h1>hotel/index</h1>
+<h1>Hoteles</h1>
+<hr>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<?php foreach ($hotels as $hotel):?>
+    <div class="row">
+      <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+          <img src="https://cadencestorage.blob.core.windows.net/mondial/PartImages/SmallImages/No_Image.jpg" alt="...">
+          <div class="caption">
+            <h3><?= Html::encode("{$hotel->hotel_name}") ?></h3>
+            <h3><?= Html::encode("{$hotel->stars}") ?></h3>
+            <p>...</p>
+            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php endforeach; ?>
+
