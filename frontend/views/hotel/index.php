e3e4c12b16d1d498fe94e116ea436d620fd3193c
Eduardo May <eduardomay_20400@outlook.com>
2019-03-21 10:46:17 -0500

    #14 mejorando la vista para la pagina de hoteles

--- frontend/views/hotel/index.php
+++ frontend/views/hotel/index.php
@@ -6,19 +6,43 @@ use yii\widgets\LinkPager;
 <h1>Hoteles</h1>
 <hr>
 
-<?php foreach ($hotels as $hotel):?>
-    <div class="row">
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-          <img src="https://cadencestorage.blob.core.windows.net/mondial/PartImages/SmallImages/No_Image.jpg" alt="...">
-          <div class="caption">
-            <h3><?= Html::encode("{$hotel->hotel_name}") ?></h3>
-            <h3><?= Html::encode("{$hotel->stars}") ?></h3>
+<?php
+
+  $cont = 1;
+
+  foreach($hotels as $hotel)
+  {
+    if ($cont == 1) {
+      echo "<div class='row'>";
+    }
+
+    echo "
+      <div class='col-sm-6 col-md-4'>
+        <div class='thumbnail'>
+          <img src='https://cadencestorage.blob.core.windows.net/mondial/PartImages/SmallImages/No_Image.jpg' alt='...'>
+          <div class='caption'>
+            <h3>$hotel->hotel_name</h3>
+            <h3>$hotel->stars</h3>
             <p>...</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+            <p><a href='#' class='btn btn-primary' role='button'>Button</a> <a href='#' class='btn btn-default' role='button'>Button</a></p>
           </div>
         </div>
       </div>
-    </div>
-<?php endforeach; ?>
+    ";
 
+    $cont++;
+
+    if ($cont == 4) {
+      echo "</div>";
+      $cont = 1;
+    }
+
+  }
+
+?>
+
+<div class="row">
+  <div class="col-md-12">
+    <?= LinkPager::widget(['pagination' => $pagination,]); ?>
+  </div>
+</div>
