a77756679b6e008cd2dbf41b3a4191e242e36627
MitchCastro <mitch.jesus32@gmail.com>
2019-03-26 12:32:10 -0500

    arreglado el problema de editar atraccion

--- backend/views/attractions/update.php
+++ backend/views/attractions/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Attractions */
 
-$this->title = . Yii::t('app','Editar Atraccion') . $model->id;
+$this->title = Yii::t('app','Editar Atraccion') . $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Atracciones', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Editar';
